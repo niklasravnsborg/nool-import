4ee18f3c2847a017f813ef536e70cef5c62faff3
Niklas Ravnsborg-Gjertsen <niklasravnsborg@gmail.com>
2017-01-24 19:10:34 +0100

    init

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+require 'vendor/autoload.php';
+use PHPHtmlParser\Dom;
+
+header('Access-Control-Allow-Origin: *');
+
+if (!isset($_FILES['file'])) {
+	echo 'No file!';
+	return;
+}
+
+$file_content = file_get_contents($_FILES['file']['tmp_name']);
+
+$html = iconv('Windows-1252', 'UTF-8', $file_content);
+$messages = [];
+
+$dom = new Dom;
+$dom->setOptions(['enforceEncoding' => 'UTF-8']);
+$dom->load($html);
+
+$date_raw = $dom->find('.mon_title')[0]->text(); // Date
+$date = date('c', strtotime(split(' ', $date_raw)[0]));
+
+$message_table = $dom->find('.mon_list')[0];
+$message_rows  = $message_table->find('tr');
+
+foreach ($message_rows as $message_row) {
+
+	// catch error on no match
+	if (!isset($message_row->find('td')[0])) {
+		continue;
+	}
+
+	$columns = $message_row->find('td');
+
+	// $teacher     = $columns[2]->text;
+	// $replacement = $columns[3]->text;
+	// $room        = $columns[4]->text;
+	// $info        = $columns[6]->text;
+
+	if ($columns[5]->text == 'Entfall') {
+		$course = strtolower($columns[0]->text);
+		$lesson = (int) $columns[1]->text;
+		$type   = 'canceled';
+	} else {
+		continue;
+	}
+
+	$messages[] = [
+		'course' => $course,
+		'lesson' => $lesson,
+		'date'   => $date,
+		'type'   => $type
+	];
+}
+
+echo json_encode($messages);
